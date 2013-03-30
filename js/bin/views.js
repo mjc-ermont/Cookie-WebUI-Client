12f87701089cb6f8e9cd8c31d2b68ef145e80cfd
Arthur <arthurtoussaint@free.fr>
2013-03-30 11:20:19 +0100

    Ajout d'un bouton de localisation sur la carte

--- js/bin/views.js
+++ js/bin/views.js
@@ -53,12 +53,14 @@ var views = {
 				hauteur = window.innerHeight;
 			else if (document.documentElement && document.documentElement.clientHeight)
 				hauteur = document.documentElement.clientHeight;
+				
 			$("#content").html("<div id='map' style='height:" + (hauteur-200) + "px'></div>");
 			map = L.map('map').setView([51.505, -0.09], 13);
 			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map);
 			L.control.scale().addTo(map);
+			L.control.locate().addTo(map);
 		},
 		callback: function(i, j, view, data) {
 
