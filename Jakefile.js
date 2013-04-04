ab661f1dac14c6d1492fd84c45ed4553afd3e713
Arthur <spam.00456@gmail.com>
2013-04-04 14:16:05 -0400

    utilisation de minify pour minifier qui permet de gerer aussi le css et le html

--- Jakefile.js
+++ Jakefile.js
@@ -1,22 +1,15 @@
 var write = require("fs").writeFileSync,
+    read = require("fs").writeFileSync,
     FileList = require("jake").FileList,
-    UglifyJS = require("uglify-js2");
+    minify = require("minify");
     
 
-var files_js = ["js/*.js", "js/vendor/*.js", "js/conf/*.js", "js/bin/*.js"]
+var files_js = ["js/*.js", "js/vendor/*.js", "js/conf/*.js", "js/bin/*.js"];
+var files_css = ["css/*.css"];
 
-task("default", ["build/min.js"]);
-directory("build");
+task("default", ["min_js"]);
 
-
-var deps_js = (new FileList(files_js)).toArray();
-deps_js.unshift("build");
-
-file("build/min.js", deps_js, function(){
+task("min_js", (new FileList(files_js)).toArray(), function(){
     var result = "";
-
-    result = UglifyJS.minify(new FileList(files_js));
-
-    write("build/min.js", result.code);
-    //return result.code;
+    result = minify.optimize(new FileList(files_js));
 });
