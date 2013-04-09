34160edfdbef6d9eb9f2fee092c9a24a210dcb0f
Arthur <spam.00456@gmail.com>
2013-04-09 10:44:26 -0400

    Copie des images des css dans le dossier de build

--- Jakefile.js
+++ Jakefile.js
@@ -2,17 +2,18 @@ var write = require("fs").writeFileSync,
     read = require("fs").readFileSync,
     FileList = require("jake").FileList,
     UglifyJS = require("uglify-js2"),
-    cleanCSS = require('clean-css')
+    cleanCSS = require('clean-css'),
+    copy = require("jake").cpR;
 
 var files_js = ["js/*-vendor/*.js", "js/bin/*.js", "js/conf/*.js", "js/*.js"];
 var files_css = ["css/*.css"];
 
-task("default", ["build", "min_js", "min_css"]);
+task("default", ["build", "min_js", "min_css", "copy_img"]);
 
 task("min_js", (new FileList(files_js)).toArray(), function(){
-        var result = UglifyJS.minify((new FileList(files_js)).sort());
-        write("build/min.js", result.code);
-        console.log("Minified JS");
+    var result = UglifyJS.minify((new FileList(files_js)).sort());
+    write("build/min.js", result.code);
+    console.log("Minified JS");
 });
 
 task("min_css", (new FileList(files_css)).toArray(), function(){
@@ -22,6 +23,11 @@ task("min_css", (new FileList(files_css)).toArray(), function(){
     });
     write("build/min.css", result);
     console.log("Minified CSS");
+});
+
+task("copy_img", (new FileList("css/images/*")).toArray(), function(){
+    copy("css/images", "build");
+    //console.log("Images copied");
 });
 
 directory("build");
